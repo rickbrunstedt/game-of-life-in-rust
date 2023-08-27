15b23a64f53bf989c625382b69c6c6bb23b2af4e
Rick <rbrunstedt@gmail.com>
2023-08-27 18:15:07 +0200

    WIP - checking and changing state

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     let mut board = create_board();
     board = set_initial_state(board);
@@ -6,16 +5,42 @@ fn main() {
 }
 
 const BOARD_SIZE: usize = 5;
-type Board = [[bool; 5]; 5];
+type Board = [[Cell; 5]; 5];
 
-fn create_board() -> Board  {
-    return [[false; BOARD_SIZE]; BOARD_SIZE];
+#[derive(Copy, Clone, PartialEq)]
+struct Cell {
+    alive: bool,
+}
+
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Cell {
+    fn check_and_change_state(self: Self, coordinate: Coordinate, board: &Board) {
+        let x: Vec<bool> = vec![|| -> bool {
+            let row = board.get(coordinate.x - 1);
+            if row.is_none() {
+                return false;
+            }
+            let cell = row.unwrap().get(coordinate.y - 1);
+            if cell.is_none() {
+                return false;
+            }
+            return cell.unwrap().alive;
+        }()];
+    }
+}
+
+fn create_board() -> Board {
+    return [[Cell { alive: false }; BOARD_SIZE]; BOARD_SIZE];
 }
 
 fn draw_board(board: Board) {
     for row in board {
         for col in row {
-            if col {
+            if col.alive {
                 print!("X");
             } else {
                 print!("_");
@@ -27,8 +52,8 @@ fn draw_board(board: Board) {
 
 fn set_initial_state(board: Board) -> Board {
     let mut new_board = board.clone();
-    new_board[2][1] = true;
-    new_board[2][2] = true;
-    new_board[2][3] = true;
-   return new_board; 
+    new_board[2][1].alive = true;
+    new_board[2][2].alive = true;
+    new_board[2][3].alive = true;
+    return new_board;
 }
