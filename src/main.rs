a8cd576e01e9a407715ac5aac0e40ef2fc04376e
Rick <rbrunstedt@gmail.com>
2023-08-27 19:01:45 +0200

    still WIP - but going somewhere..

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ fn main() {
     let mut board = create_board();
     board = set_initial_state(board);
     draw_board(board);
+    let theone = board[2][1];
+
+    // dbg!(theone.alive);
+    // dbg!(theone.alive);
+    // assert!(board[2][3].alive == false);
+    dbg!(board[2][1]);
+    theone.check_and_change_state(Coordinate { x: 2, y: 1 }, &board);
+    dbg!(board[2][1]);
+    // dbg!(theone.alive);
+    //assert!(board[2][3].alive == true);
 }
 
 const BOARD_SIZE: usize = 5;
 type Board = [[Cell; 5]; 5];
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 struct Cell {
     alive: bool,
 }
@@ -18,18 +28,114 @@ struct Coordinate {
 }
 
 impl Cell {
-    fn check_and_change_state(self: Self, coordinate: Coordinate, board: &Board) {
-        let x: Vec<bool> = vec![|| -> bool {
-            let row = board.get(coordinate.x - 1);
-            if row.is_none() {
-                return false;
+    fn check_and_change_state(mut self, coordinate: Coordinate, board: &Board) {
+        let neighbours: Vec<bool> = vec![
+            || -> bool {
+                let row = board.get(coordinate.x - 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y - 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x - 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x - 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y + 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y - 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y + 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x + 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y - 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x + 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+            || -> bool {
+                let row = board.get(coordinate.x + 1);
+                if row.is_none() {
+                    return false;
+                }
+                let cell = row.unwrap().get(coordinate.y + 1);
+                if cell.is_none() {
+                    return false;
+                }
+                return cell.unwrap().alive;
+            }(),
+        ];
+        dbg!(&neighbours);
+
+        let alive_neighbours = neighbours.iter().filter(|val| **val).count();
+        if self.alive {
+            if alive_neighbours > 2 || alive_neighbours < 3 {
+                self.alive = false;
+            } else {
+                self.alive = true;
             }
-            let cell = row.unwrap().get(coordinate.y - 1);
-            if cell.is_none() {
-                return false;
+        } else {
+            if alive_neighbours == 3 {
+                self.alive = true;
+            } else {
+                self.alive = false;
             }
-            return cell.unwrap().alive;
-        }()];
+        }
+        dbg!(self.alive);
     }
 }
 
